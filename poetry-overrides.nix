b1c2b098cf9bf1e81c0d23eaea7074e81a19b81e
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-11-28 06:58:46 -0500

    style: rename self -> pyself

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }:
-self: pysuper:
+pyself: pysuper:
 {
   typing-extensions = pysuper.typing-extensions.overridePythonAttrs (attrs: {
-    buildInputs = (attrs.buildInputs or [ ]) ++ [ self.flit-core ];
+    buildInputs = (attrs.buildInputs or [ ]) ++ [ pyself.flit-core ];
   });
 
   protobuf = pysuper.protobuf.overridePythonAttrs (attrs: {
-    nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ [ self.pyext ];
+    nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ [ pyself.pyext ];
     propagatedNativeBuildInputs = (attrs.propagatedNativeBuildInputs or [ ]) ++ [
       pkgs.buildPackages.protobuf
     ];
@@ -17,13 +17,13 @@ self: pysuper:
     '';
 
     preBuild = ''
-      ${self.python.pythonForBuild.interpreter} setup.py build
-      ${self.python.pythonForBuild.interpreter} setup.py build_ext --cpp_implementation
+      ${pyself.python.pythonForBuild.interpreter} setup.py build
+      ${pyself.python.pythonForBuild.interpreter} setup.py build_ext --cpp_implementation
     '';
 
     installFlags = "--install-option='--cpp_implementation'";
     postInstall = ''
-      cp -v $(find build -name "_message*") $out/${self.python.sitePackages}/google/protobuf/pyext
+      cp -v $(find build -name "_message*") $out/${pyself.python.sitePackages}/google/protobuf/pyext
     '';
   });
 }
