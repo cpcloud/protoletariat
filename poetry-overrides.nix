87a8ae1694449de0cd586feed50aace1f9ea31c9
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-11-28 06:54:43 -0500

    perf: use cpp implementation for python protobuf

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,7 +1,29 @@
-{ ... }:
-_: super:
+{ pkgs, ... }:
+self: super:
 {
   typing-extensions = super.typing-extensions.overridePythonAttrs (attrs: {
     buildInputs = (attrs.buildInputs or [ ]) ++ [ super.flit-core ];
+  });
+
+  protobuf = super.protobuf.overridePythonAttrs (attrs: {
+    nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ [ self.pyext ];
+    propagatedNativeBuildInputs = (attrs.propagatedNativeBuildInputs or [ ]) ++ [
+      pkgs.buildPackages.protobuf
+    ];
+    buildInputs = (attrs.buildInputs or [ ]) ++ [ pkgs.buildPackages.protobuf ];
+    preConfigure = ''
+      export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+      export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
+    '';
+
+    preBuild = ''
+      ${self.python.pythonForBuild.interpreter} setup.py build
+      ${self.python.pythonForBuild.interpreter} setup.py build_ext --cpp_implementation
+    '';
+
+    installFlags = "--install-option='--cpp_implementation'";
+    postInstall = ''
+      cp -v $(find build -name "_message*") $out/${self.python.sitePackages}/google/protobuf/pyext
+    '';
   });
 }
