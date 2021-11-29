2a043593c367eaf723cbf662237b895bebe71247
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-11-29 17:39:42 -0500

    test: add failing test

--- protoletariat/tests/conftest.py
+++ protoletariat/tests/conftest.py
@@ -497,6 +497,50 @@ def buf_gen_yaml_long_names(tmp_path: Path, out_long_names: Path) -> Path:
     return p
 
 
+@pytest.fixture(scope="session")
+def google_import_text() -> str:
+    return """
+syntax = "proto3";
+
+import "google/protobuf/empty.proto";
+
+message Foo {}
+"""
+
+
+@pytest.fixture
+def out_google_import(tmp_path: Path) -> Path:
+    out = tmp_path / "out_google_import"
+    out.mkdir()
+    return out
+
+
+@pytest.fixture
+def google_import_proto(tmp_path: Path, google_import_text: str) -> Path:
+    p = tmp_path / "google_import.proto"
+    p.write_text(google_import_text)
+    return p
+
+
+@pytest.fixture
+def buf_gen_yaml_google_import(tmp_path: Path, out_google_import: Path) -> Path:
+    p = tmp_path / "buf.gen.yaml"
+    p.write_text(
+        json.dumps(
+            {
+                "version": "v1",
+                "plugins": [
+                    {
+                        "name": "python",
+                        "out": str(out_google_import),
+                    },
+                ],
+            },
+        )
+    )
+    return p
+
+
 def check_import_lines(result: Result, expected_lines: Iterable[str]) -> None:
     lines = result.stdout.splitlines()
     assert set(expected_lines) <= set(lines)
