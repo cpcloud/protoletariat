ce3cda2dc1c0b343a29692427570d53a1efd1bc2
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-12-12 07:59:49 -0500

    chore: actually use poetry2nix flake

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,29 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 pyself: pysuper:
 {
   typing-extensions = pysuper.typing-extensions.overridePythonAttrs (attrs: {
     buildInputs = (attrs.buildInputs or [ ]) ++ [ pyself.flit-core ];
-  });
-
-  protobuf = pysuper.protobuf.overridePythonAttrs (attrs: {
-    nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ [ pyself.pyext ];
-    propagatedNativeBuildInputs = (attrs.propagatedNativeBuildInputs or [ ]) ++ [
-      pkgs.buildPackages.protobuf
-    ];
-    buildInputs = (attrs.buildInputs or [ ]) ++ [ pkgs.buildPackages.protobuf ];
-    preConfigure = ''
-      export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
-      export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
-    '';
-
-    preBuild = ''
-      ${pyself.python.pythonForBuild.interpreter} setup.py build
-      ${pyself.python.pythonForBuild.interpreter} setup.py build_ext --cpp_implementation
-    '';
-
-    installFlags = "--install-option='--cpp_implementation'";
-    postInstall = ''
-      cp -v $(find build -name "_message*") $out/${pyself.python.sitePackages}/google/protobuf/pyext
-    '';
   });
 }
