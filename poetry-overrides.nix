dbd882615990fa811ea6429439c069faa3c4bec0
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2021-12-27 09:32:21 -0500

    chore: remove poetry-overrides

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,7 +1,1 @@
-{ ... }:
-pyself: pysuper:
-{
-  typing-extensions = pysuper.typing-extensions.overridePythonAttrs (attrs: {
-    buildInputs = (attrs.buildInputs or [ ]) ++ [ pyself.flit-core ];
-  });
-}
+{ ... }: _: _: { }
