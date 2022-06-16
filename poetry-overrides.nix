727fc0c431c5126c9852d26a6c3bc3edee06ddcb
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-06-16 13:58:15 -0400

    build: add setuptools to python deps that require it

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,9 +1,19 @@
 { ... }: self: super: {
-  platformdirs = super.platformdirs.overridePythonAttrs (attrs: {
-    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.setuptools ];
+  hatch-vcs = super.hatch-vcs.overridePythonAttrs (attrs: {
+    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [
+      self.setuptools
+    ];
   });
 
   pytest-runner = super.pytest-runner.overridePythonAttrs (attrs: {
-    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.setuptools ];
+    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [
+      self.setuptools
+    ];
+  });
+
+  platformdirs = super.platformdirs.overridePythonAttrs (attrs: {
+    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [
+      self.setuptools
+    ];
   });
 }
