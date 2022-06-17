93037b8ea4a56c6b7a48e2f70fb8b736a4a89197
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-06-17 05:24:53 -0500

    chore(deps): override poetry packages

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,1 +1,9 @@
-{ ... }: _: _: { }
+{ ... }: self: super: {
+  platformdirs = super.platformdirs.overridePythonAttrs (attrs: {
+    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.setuptools ];
+  });
+
+  pytest-runner = super.pytest-runner.overridePythonAttrs (attrs: {
+    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.setuptools ];
+  });
+}
