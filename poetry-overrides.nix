cecedccacaa815364da602215893fb680b1519a0
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-06-17 06:28:07 -0400

    style: sort dependencies

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -5,13 +5,13 @@
     ];
   });
 
-  pytest-runner = super.pytest-runner.overridePythonAttrs (attrs: {
+  platformdirs = super.platformdirs.overridePythonAttrs (attrs: {
     nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [
       self.setuptools
     ];
   });
 
-  platformdirs = super.platformdirs.overridePythonAttrs (attrs: {
+  pytest-runner = super.pytest-runner.overridePythonAttrs (attrs: {
     nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [
       self.setuptools
     ];
