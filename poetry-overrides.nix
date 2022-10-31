d81cd5d884e2284dd29cf93fa479eaa333904516
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-10-31 10:01:33 -0400

    chore: ignore 3.8-only mypy build warning

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -6,4 +6,8 @@
   exceptiongroup = super.exceptiongroup.overridePythonAttrs (attrs: {
     nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.flit-scm ];
   });
+
+  mypy = super.mypy.overridePythonAttrs (attrs: self.lib.optionalAttrs (self.pythonOlder "3.9") {
+    NIX_CFLAGS_COMPILE = self.lib.concatStringsSep " " (attrs.NIX_CFLAGS_COMPILE or [ ] ++ [ "-Wno-error=cpp" ]);
+  });
 }
