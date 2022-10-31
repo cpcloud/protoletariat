63e59464e7d1d6c73443e11b4b3a64ea9a5b67c0
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-10-31 08:31:20 -0400

    chore: bump deps and override

--- poetry-overrides.nix
+++ poetry-overrides.nix
@@ -1,7 +1,9 @@
 { ... }: self: super: {
   grpc-stubs = super.grpc-stubs.overridePythonAttrs (attrs: {
-    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [
-      self.setuptools
-    ];
+    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.setuptools ];
+  });
+
+  exceptiongroup = super.exceptiongroup.overridePythonAttrs (attrs: {
+    nativeBuildInputs = attrs.nativeBuildInputs or [ ] ++ [ self.flit-scm ];
   });
 }
